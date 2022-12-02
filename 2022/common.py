490edc3089a3b089c1d0c4c682bdd6becea52b1f
Matt Struble <mattstruble@outlook.com>
2022-12-02 12:06:32 -0500

    First day of solutions.

---
+++ 2022/common.py
@@ -0,0 +1,33 @@
+import os 
+from typing import Generator, AnyStr
+
+def file_line_generator(path:str = None) -> Generator[AnyStr, None, None]:
+    if path is None:
+        path = "input.txt"
+
+    with open(path, 'r') as file:
+        for line in file:
+            yield line.rstrip("\r\n")
+
+class MinHeap:
+    def __init__(self, max_size: Optional[int] = None):
+        self.max_size = max_size 
+        self.heap = []
+
+    def push(self, x: Any):
+        if self.max_size and len(self) >= self.max_size:
+            heapq.heappushpop(self.heap, x)
+        else:
+            heapq.heappush(self.heap, x)
+
+    def pop(self):
+        return heapq.heappop(self.h)
+
+    def __getitem__(self, i) -> Any: 
+        return self.heap[i]
+
+    def __len__(self):
+        return len(self.heap)
+
+    def __str__(self) -> str:
+        return str(self.heap)
