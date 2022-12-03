004e8df8a61cb3a016839457c6c8e98f1737f2c4
Matt Struble <mattstruble@outlook.com>
2022-12-03 09:49:07 -0500

    Adding puzzle runner

--- 2022/common.py
+++ 2022/common.py
@@ -1,5 +1,6 @@
 import os 
-from typing import Generator, AnyStr, Optional, Any
+import sys 
+from typing import Generator, AnyStr, Optional, Any, Iterable
 
 import heapq
 from pathlib import Path
@@ -7,7 +8,10 @@ from pathlib import Path
 INPUTS_DIR = os.path.join(os.path.dirname(__file__), "inputs")
 INPUT_FILE_NAME = "input.txt"
 
-def file_line_generator(file_path, *, path:str = None) -> Generator[AnyStr, None, None]:
+def file_line_generator(file_path=None, *, path:str = None) -> Generator[AnyStr, None, None]:
+    if file_path is None:
+        file_path = sys.argv[0]
+
     if path is None:
         path = os.path.join(INPUTS_DIR, Path(file_path).stem, INPUT_FILE_NAME)
 
@@ -37,3 +41,34 @@ class MinHeap:
 
     def __str__(self) -> str:
         return str(self.heap)
+
+class PuzzleRunner: 
+
+    def __init__(self) -> None:
+        self._puzzle_funcs = [
+            self.puzzle_one,
+            self.puzzle_two
+        ]
+
+    def get_example(self) -> Iterable[AnyStr]:
+        raise NotImplementedError
+
+    def puzzle_one(self, data: Iterable[str]) -> int:
+        raise NotImplementedError
+
+    def puzzle_two(self, data: Iterable[str]) -> int:
+        raise NotImplementedError
+
+    def _run(self, data: Iterable[str]) -> None:
+        for puzzle_func in self._puzzle_funcs:
+            try: 
+                result = puzzle_func(data)
+                print(f"{puzzle_func.__qualname__}: {result}")
+            except NotImplementedError: 
+                print(f"{puzzle_func.__qualname__} not Implemented")
+
+    def run(self):
+        self._run(file_line_generator())
+
+    def test(self):
+        self._run(self.get_example())
