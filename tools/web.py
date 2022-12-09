1cafe9c8df5e09d5faa04d04873f469417f2dee3
Matt Struble <mattstruble@outlook.com>
2022-12-09 13:57:25 -0500

    feat: submit solutions

--- tools/web.py
+++ tools/web.py
@@ -2,7 +2,7 @@
 import json
 import os
 import time
-from typing import Optional
+from typing import Optional, Union
 
 import requests
 
@@ -10,14 +10,13 @@ from tools import CACHE_DIR, INPUT_FILE_NAME, INPUTS_DIR
 from tools.config import get_configuration
 from tools.utils import Singleton
 
-# curl 'https://adventofcode.com/2022/day/7/answer' -X POST -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: https://adventofcode.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://adventofcode.com/2022/day/7' -H 'Cookie: session=XXX' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-User: ?1' -H 'Sec-GPC: 1' --data-raw 'level=2&answer=3866390'
-
 
 class RequestLimiter(Singleton):
     """Be kind, and don't spam adventofcode.com"""
 
     GENERAL_RATE_SEC = 1
-    DUPLICATE_RATE_SEC = 10
+    GET_RATE_SEC = 10
+    POST_RATE_SEC = 60
 
     def __init__(self) -> None:
         try:
@@ -32,11 +31,11 @@ class RequestLimiter(Singleton):
         with open(os.path.join(CACHE_DIR, "requests.json"), "w") as f:
             json.dump(self.requests, f)
 
-    def get(self, url, **kwargs):
+    def _wait(self, url: str, max_wait: int):
         now = time.time()
 
         if url in self.requests:
-            time.sleep(max(0, self.DUPLICATE_RATE_SEC - (now - self.requests[url])))
+            time.sleep(max(0, max_wait - (now - self.requests[url])))
         else:
             time.sleep(max(0, self.GENERAL_RATE_SEC - (now - self.last_request_time)))
 
@@ -44,6 +43,13 @@ class RequestLimiter(Singleton):
         self.last_request_time = time.time()
 
         self._save()
+
+    def get(self, url, **kwargs):
+        self._wait(url, self.GET_RATE_SEC)
+        return requests.get(url, **kwargs)
+
+    def post(self, url, **kwargs):
+        self._wait(url, self.POST_RATE_SEC)
         return requests.get(url, **kwargs)
 
 
@@ -70,12 +76,38 @@ class AOCWebInterface:
         if os.path.exists(path):
             return
 
-        response = self.request_limit.get(
-            f"https://adventofcode.com/{self.year}/day/{self.day}/input",
-            **self.request_kwargs,
-        )
+        url = f"https://adventofcode.com/{self.year}/day/{self.day}/input"
+
+        response = self.request_limit.get(url, **self.request_kwargs)
 
         os.makedirs(os.path.dirname(path), exist_ok=True)
 
         with open(path, "wb") as f:
             f.write(response.content)
+
+    def submit(self, puzzle_num: int, solution: Union[str, int, float]) -> bool:
+        data = {"level": puzzle_num, "answer": solution}
+
+        headers = {
+            "Origin": "https://adventofcode.com",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Referer": f"https://adventofcode.com/{self.year}/day/{self.day}",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+        }
+
+        url = f"https://adventofcode.com/{self.year}/day/{self.day}/answer"
+
+        response = self.request_limit.post(
+            url, data=data, headers=headers, **self.request_kwargs
+        )
+
+        print(response.content)
+
+        # hack for now, will change when get live response next day
+        return (
+            "that is right!" in response.content.lower()
+            and "gold star" in response.content.lower()
+        )
