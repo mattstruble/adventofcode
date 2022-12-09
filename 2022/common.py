c3554880713be7360cb7396e0a17ca035bcafc3b
Matt Struble <mattstruble@outlook.com>
2022-12-09 10:04:53 -0500

    refactor: move cfg to parent dir

--- 2022/common.py
+++ 2022/common.py
@@ -27,7 +27,7 @@ def str_to_ints(string: str) -> List[int]:
 @lru_cache(maxsize=1)
 def get_configuration() ->configparser.ConfigParser:
     config = configparser.ConfigParser()
-    config.read(os.path.join(os.path.dirname(__file__), "puzzle.cfg"))
+    config.read("adventofcode.cfg")
 
     return config 
 
